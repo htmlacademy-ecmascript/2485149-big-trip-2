6223127bb2ec5f8174888e28a7fef0ae617970fe
kristijs <ogospadi@icloud.com>
2024-12-12 17:01:56 +0300

    TripPresenter

--- src/main.js
+++ src/main.js
@@ -1,18 +1,7 @@
-import FilterView from './view/filter-view.js';
-// import Presenter from './presenter/presenter.js';
-import TripPresenter from './presenter/tpipPresenter.js';
-import {render} from './render.js';
+import TripPresenter from './presenter/TripPresenter';
 
-const siteHeader = document.querySelector('.page-header');
-const siteFilterContainer = siteHeader.querySelector('.trip-controls__filters');
-const siteMain = document.querySelector('.page-body__page-main');
-const siteSortingContainer = siteMain.querySelector('.trip-events');
+const siteSortingContainer = document.querySelector('.trip-events');
 
-// const presenter = new Presenter({container: siteSortingContainer});
-const tripPresenter = new TripPresenter({container: siteSortingContainer});
-
-render(new FilterView(), siteFilterContainer);
-
-// presenter.init();
+const tripPresenter = new TripPresenter(siteSortingContainer);
 tripPresenter.init();
 
---
+++ src/presenter/TripPresenter.js
@@ -0,0 +1,76 @@
+import InfoTripView from '../view/info-trip-view';
+import FilterView from '../view/filter-view';
+import SortView from '../view/sort-view';
+import EditList from '../view/event-list-view';
+import FormEditView from '../view/form-edit-view';
+import PointView from '../view/point-view';
+import { render } from '../render';
+import { mockData } from '../mock/mockData';
+import { mockDestination } from '../mock/destinations';
+import { mockOffers } from '../mock/offers';
+import PointsModal from '../modals/trip-points';
+
+export default class TripPresenter {
+  constructor(container) {
+    this.container = container;
+    this.data = mockData;
+    this.pointsModal = new PointsModal(mockData);
+    this.routeListPoints = new EditList();
+    this.sortView = new SortView();
+    this.formEdit = new FormEditView();
+  }
+
+  init() {
+    const tripInfo = {
+      title: this.getTripTitle(this.data),
+      dates: this.getTripDates(this.data),
+      cost: this.getTotalCost(this.data),
+    };
+    const infoTripView = new InfoTripView(tripInfo);
+    render(infoTripView, this.container);
+
+    const filterView = new FilterView();
+    render(filterView, this.container);
+
+    render(this.sortView, this.container);
+    render(this.formEdit, this.container);
+    render(this.routeListPoints, this.container);
+    const points = this.pointsModal.getPoints();
+    points.forEach((point) => {
+      const destination = mockDestination.find((dest) => dest.id === point.destination);
+      const offers = point.offers.map((offerId) =>
+        mockOffers.find((offer) => offer.id === offerId)
+      );
+
+      const pointView = new PointView(point, destination, offers);
+      render(pointView, this.routeListPoints.getElement());
+    });
+  }
+
+  getTripTitle(data) {
+    const destinationNames = data.map((point) => {
+      const destination = mockDestination.find((dest) => dest.id === point.destination);
+      return destination ? destination.name : 'Unknown';
+    });
+    return destinationNames.join(' - ');
+  }
+
+  getTripDates(data) {
+    if (!data.length) {
+      return '';
+    }
+    const startDate = new Date(data[0].dateFrom).toLocaleDateString('en-GB', { day: '2-digit', month: 'short', year: 'numeric' });
+    const endDate = new Date(data[data.length - 1].dateTo).toLocaleDateString('en-GB', { day: '2-digit', month: 'short', year: 'numeric' });
+    return `${startDate} - ${endDate}`;
+  }
+
+  getTotalCost(data) {
+    return data.reduce((total, point) => {
+      const offersCost = point.offers.reduce((sum, offerId) => {
+        const offer = mockOffers.find((theOffer) => theOffer.id === offerId);
+        return sum + (offer ? offer.price : 0);
+      }, 0);
+      return total + point.basePrice + offersCost;
+    }, 0);
+  }
+}
