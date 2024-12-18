2458ff117eaf8d2a30c9c055ab4234c9588904d5
kristijs <ogospadi@icloud.com>
2024-12-18 21:07:32 +0300

    version 2

--- src/presenter/TripPresenter.js
+++ src/presenter/TripPresenter.js
@@ -4,55 +4,38 @@ import SortView from '../view/sort-view';
 import EditList from '../view/event-list-view';
 import FormEditView from '../view/form-edit-view';
 import PointView from '../view/point-view';
+import { render } from '../render';
 import { mockData } from '../mock/mockData';
 import { mockDestination } from '../mock/destinations';
 import { mockOffers } from '../mock/offers';
 import PointsModal from '../modals/trip-points';
-import { render,replace,RenderPosition } from '../framework/render';
-import ButtonNewEventView from '../view/button-new-event-view';
-import { MessageText } from '../const';
 
 export default class TripPresenter {
-  // constructor(container) {
-  //   this.#container = container;
-  //   this.#data = mockData;
-  //   this.#pointsModal = new PointsModal(mockData);
-  //   this.#routeListPoints = new EditList();
-  //   this.#sortView = new SortView();
-  //   this.#formEdit = new FormEditView();
-  #container;
-  #data;
-  #pointsModal = new PointsModal(mockData);
-  #routeListPoints = new EditList();
-  #buttonNewEvent = new ButtonNewEventView();
-  #sortView = new SortView();
-  #formEdit = new FormEditView();
-  #points = [];
-  #destinations = [];
-  #offers = [];
-  #filters = [];
-
-  constructor({}){
-    this.#container = container;
-    this.#data = mockData;
+  constructor(container) {
+    this.container = container;
+    this.data = mockData;
+    this.pointsModal = new PointsModal(mockData);
+    this.routeListPoints = new EditList();
+    this.sortView = new SortView();
+    this.formEdit = new FormEditView();
   }
 
   init() {
     const tripInfo = {
-      title: this.getTripTitle(this.#data),
-      dates: this.getTripDates(this.#data),
-      cost: this.getTotalCost(this.#data),
+      title: this.getTripTitle(this.data),
+      dates: this.getTripDates(this.data),
+      cost: this.getTotalCost(this.data),
     };
     const infoTripView = new InfoTripView(tripInfo);
-    render(infoTripView, this.#container);
+    render(infoTripView, this.container);
 
     const filterView = new FilterView();
-    render(filterView, this.#container);
+    render(filterView, this.container);
 
-    render(this.#sortView, this.#container);
-    render(this.#formEdit, this.#container);
-    render(this.#routeListPoints, this.#container);
-    const points = this.#pointsModal.getPoints();
+    render(this.sortView, this.container);
+    render(this.formEdit, this.container);
+    render(this.routeListPoints, this.container);
+    const points = this.pointsModal.getPoints();
     points.forEach((point) => {
       const destination = mockDestination.find((dest) => dest.id === point.destination);
       const offers = point.offers.map((offerId) =>
@@ -60,7 +43,7 @@ export default class TripPresenter {
       );
 
       const pointView = new PointView(point, destination, offers);
-      render(pointView, this.#routeListPoints.getElement());
+      render(pointView, this.routeListPoints.getElement());
     });
   }
 
